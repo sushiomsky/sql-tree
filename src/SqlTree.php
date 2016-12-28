faced3d6d97049fe5567fee629d13e8723f02d85
sushiomsky <harrybotya@gmail.com>
2016-12-29 00:56:04 +0100

    raw dev commit

--- src/SqlTree.php
+++ src/SqlTree.php
@@ -46,11 +46,11 @@ class SqlTree {
 	 * @var array QUERYS Sql querys nescessary to manage the tree structure
 	 */
 	const QUERYS = array (
-			'SELECT_WHERE_ID' => 'SELECT :id, :rgt, :lft, :parent, :name FROM :table WHERE :id=:idvalue LIMIT 0,1',
+			'SELECT_WHERE_ID' => 'SELECT :id, :rgt, :lft, :parent, :name FROM :table WHERE :id=:value_id LIMIT 0,1',
 			'SELECT_TOP_RGT' => 'SELECT :rgt FROM :table WHERE 1 ORDER BY :rgt DESC LIMIT 0,1',
-			'UPDATE_PARENTS_RGT' => 'UPDATE :table SET :rgt = :rgt +2 WHERE :rgt >= :rgtvalue',
-			'UPDATE_PARENTS_LFT' => 'UPDATE :table SET :lft = :lft +2 WHERE :lft >= :lftvalue',
-			'INSERT_NODE' => 'INSERT INTO :table (:lft, :rgt, :parent, :name) VALUES(:lftvalue, :rgtvalue, :parentvalue, :namevalue)' 
+			'UPDATE_PARENTS_RGT' => 'UPDATE :table SET :rgt = :rgt +2 WHERE :rgt >= :value_rgt',
+			'UPDATE_PARENTS_LFT' => 'UPDATE :table SET :lft = :lft +2 WHERE :lft >= :value_lft',
+			'INSERT_NODE' => 'INSERT INTO :table (:lft, :rgt, :parent, :name) VALUES(:value_lft, :value_rgt, :value_parent, :value_name)' 
 	);
 	
 	/**
@@ -93,15 +93,40 @@ class SqlTree {
 	public function addRootNode($name){
 	
 	}
-	
+
 	/**
-	 * prepares pdo statements
+	 * prepares pdo statements and bind static params such as tablename and column names
 	 */
 	private function prepareStatements() {
 		$keys = array_keys ( QUERYS );
 		foreach ( $keys as $key ) {
-			$this->statements [strtolower ( $key )] = $this->pdo->prepare ( QUERYS [$key] );
+			$loKey = strtolower ( $key );
+			$this->statements[$loKey] = $this->pdo->prepare ( QUERYS [$key] );
+
+			$this->statements[$loKey]->bindParam(':table',$this->columns['table']);
+			if (strpos(QUERYS[$key], ':id') !== false) {
+				$this->statements[$loKey]->bindParam(':id',$this->columns['id']);
+			}
+			if (strpos(QUERYS[$key], ':lft') !== false) {
+				$this->statements[$loKey]->bindParam(':lft',$this->columns['lft']);
+			}
+			if (strpos(QUERYS[$key], ':rgt') !== false) {
+				$this->statements[$loKey]->bindParam(':rgt',$this->columns['rgt']);
+			}
+			if (strpos(QUERYS[$key], ':parent') !== false) {
+				$this->statements[$loKey]->bindParam(':parent',$this->columns['parent']);
+			}
+			if (strpos(QUERYS[$key], ':name') !== false) {
+				$this->statements[$loKey]->bindParam(':name',$this->columns['name']);
+			}
 		}
+	}
+
+	/**
+	 * Validates tree consistency
+	 */
+	private function validateTree() {
+		return false;
 	}
 	
 	/**
